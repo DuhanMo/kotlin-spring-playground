b1b3817097f54e35399b9cfc27122e2de6cf6890
Duhan Mo <duhandv@gmail.com>
2024-03-08 16:57:21 +0900

    Wip

--- src/main/kotlin/org/example/kotlinspringplayground/KotlinSpringPlaygroundApplication.kt
+++ src/main/kotlin/org/example/kotlinspringplayground/KotlinSpringPlaygroundApplication.kt
@@ -1,11 +1,36 @@
 package org.example.kotlinspringplayground
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.client.RestClient
 
 @SpringBootApplication
 class KotlinSpringPlaygroundApplication
 
 fun main(args: Array<String>) {
     runApplication<KotlinSpringPlaygroundApplication>(*args)
+    val restClient = RestClient.create()
+    val items: IntRange = 1..150
+    val start = System.currentTimeMillis()
+    runBlocking {
+        withContext(Dispatchers.IO) {
+            val deferreds = items.map {
+                async {
+                    restClient.get()
+                        .uri("http://localhost:8081/{id}", it)
+                        .retrieve()
+                        .body(String::class.java)
+                }
+            }
+            val awaitAll = deferreds.awaitAll()
+            awaitAll.forEach { println(it) }
+        }
+    }
+    val end = System.currentTimeMillis()
+    println("total time: ${end - start}" )
 }
