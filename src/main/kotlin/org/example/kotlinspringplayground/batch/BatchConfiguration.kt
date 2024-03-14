d2e8a9dfe636bfcf71c8bcd72aad8b6a2b4f5a18
Duhan Mo <duhandv@gmail.com>
2024-03-14 17:30:51 +0900

    Spring batch get started

--- src/main/kotlin/org/example/kotlinspringplayground/batch/BatchConfiguration.kt
+++ src/main/kotlin/org/example/kotlinspringplayground/batch/BatchConfiguration.kt
@@ -1,43 +1,68 @@
 package org.example.kotlinspringplayground.batch
 
-import org.slf4j.LoggerFactory
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
-import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
-import org.springframework.batch.repeat.RepeatStatus
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.batch.item.database.JdbcBatchItemWriter
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder
+import org.springframework.batch.item.file.FlatFileItemReader
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
 import org.springframework.jdbc.datasource.DataSourceTransactionManager
+import javax.sql.DataSource
 
 @Configuration
 class BatchConfiguration {
-    companion object {
-        private val logger = LoggerFactory.getLogger(BatchConfiguration::class.java)
-    }
     @Bean
-    fun simpleJob(jobRepository: JobRepository, simpleStep: Step): Job {
-        return JobBuilder("simpleJob", jobRepository)
-            .start(simpleStep)
+    fun reader(): FlatFileItemReader<Person> {
+        return FlatFileItemReaderBuilder<Person>()
+            .name("personItemReader")
+            .resource(ClassPathResource("/sample-data.csv"))
+            .delimited()
+            .names("firstName", "lastName")
+            .targetType(Person::class.java)
             .build()
     }
 
     @Bean
-    @JobScope
-    fun simpleStep(
-        @Value("#{jobParameters[requestDate]}") requestDate: String,
+    fun processor(): PersonItemProcessor {
+        return PersonItemProcessor()
+    }
+
+    @Bean
+    fun writer(dataSource: DataSource): JdbcBatchItemWriter<Person> {
+        return JdbcBatchItemWriterBuilder<Person>()
+            .sql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)")
+            .dataSource(dataSource)
+            .beanMapped()
+            .build()
+    }
+
+    @Bean
+    fun importUserJob(jobRepository: JobRepository, step1: Step, listener: JobCompletionNotificationListener): Job {
+        return JobBuilder("importUserJob", jobRepository)
+            .listener(listener)
+            .start(step1)
+            .build()
+    }
+
+    @Bean
+    fun step1(
         jobRepository: JobRepository,
         transactionManager: DataSourceTransactionManager,
+        reader: FlatFileItemReader<Person>,
+        processor: PersonItemProcessor,
+        writer: JdbcBatchItemWriter<Person>
     ): Step {
-        return StepBuilder("simpleStep", jobRepository)
-            .tasklet({ _, _ ->
-                logger.info(">>> This is simpleStep")
-                logger.info(">>> requestDate = $requestDate")
-                RepeatStatus.FINISHED
-            }, transactionManager)
+        return StepBuilder("step1", jobRepository)
+            .chunk<Person, Person>(3, transactionManager)
+            .reader(reader)
+            .processor(processor)
+            .writer(writer)
             .build()
     }
 }
