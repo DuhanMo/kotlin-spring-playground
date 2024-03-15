3ad7b25cb8dfadc290596b793d8128e9f0fe4914
Duhan Mo <duhandv@gmail.com>
2024-03-15 15:22:07 +0900

    Spring batch Job Flow

--- src/main/kotlin/org/example/kotlinspringplayground/batch/BatchConfiguration.kt
+++ src/main/kotlin/org/example/kotlinspringplayground/batch/BatchConfiguration.kt
@@ -3,10 +3,12 @@ package org.example.kotlinspringplayground.batch
 import org.slf4j.LoggerFactory
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.datasource.DataSourceTransactionManager
@@ -17,20 +19,23 @@ class BatchConfiguration {
         private val logger = LoggerFactory.getLogger(BatchConfiguration::class.java)
     }
     @Bean
-    fun job(jobRepository: JobRepository, step1: Step): Job {
-        return JobBuilder("job", jobRepository)
-            .start(step1)
+    fun simpleJob(jobRepository: JobRepository, simpleStep: Step): Job {
+        return JobBuilder("simpleJob", jobRepository)
+            .start(simpleStep)
             .build()
     }
 
     @Bean
-    fun step1(
+    @JobScope
+    fun simpleStep(
+        @Value("#{jobParameters[requestDate]}") requestDate: String,
         jobRepository: JobRepository,
         transactionManager: DataSourceTransactionManager,
     ): Step {
-        return StepBuilder("step1", jobRepository)
+        return StepBuilder("simpleStep", jobRepository)
             .tasklet({ _, _ ->
-                logger.info("This is step1")
+                logger.info(">>> This is simpleStep")
+                logger.info(">>> requestDate = $requestDate")
                 RepeatStatus.FINISHED
             }, transactionManager)
             .build()
