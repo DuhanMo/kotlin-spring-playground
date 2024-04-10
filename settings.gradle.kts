a3541eac5fa9a792868b70466f202995ac0c1100
Duhan Mo <duhandv@gmail.com>
2024-04-10 21:30:11 +0900

    chore: Move directory for independent project

--- settings.gradle.kts
+++
@@ -1,6 +0,0 @@
-rootProject.name = "kotlin-spring-playground"
-include("spring-batch")
-include("coroutine")
-include("security-normal-cookie")
-include("security-oauth2")
-include("multiple-datasource")
